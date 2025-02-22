2fd8d69f2062442f92fc4049bffbb60956285470
Dhruv Jalan <aamerjalan2@gmail.com>
2025-02-22 12:50:24 +0530

    connect, send, recv

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 int main(){
 
@@ -16,8 +18,17 @@ int main(){
     int result = connect(socketFD, (struct sockaddr*)&address, sizeof address);
 
     if(result ==  0){
-       printf("connection was successful");
+       printf("connection was successful\n");
     }
+
+    char* message;
+    message = "GET \\ HTTP/1.1\r\nHost:google.com\r\n\r\n";
+    send(socketFD, message, strlen(message), 0);
+
+    char buffer[1024];
+    recv(socketFD, buffer, 1024, 0);
+
+    printf("Response was %s\n", buffer);
     
     return 0;
 }
